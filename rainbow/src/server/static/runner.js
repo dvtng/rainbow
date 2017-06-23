d5e2f592c6e81276f816b2a2ed2faba82864007d
David Tang <tang.ddt@gmail.com>
2017-06-23 16:45:33 +1000

    Allow changing between stories in a file

--- rainbow/src/server/static/runner.js
+++ rainbow/src/server/static/runner.js
@@ -1,3 +1,34 @@
-var Component = window.story.default;
+var React = window.React;
+var ReactDOM = window.ReactDOM;
+var story = window.story;
 
-window.ReactDOM.render(Component, document.getElementById('root'));
+var rootEl = document.getElementById('root');
+
+function error(message) {
+    rootEl.textContent = message;
+}
+
+function renderStory() {
+    var storyName = window.location.hash.length > 1
+        ? window.location.hash.substr(1)
+        : 'default';
+    var Component = story[storyName];
+
+    if (Component == null) {
+        error('Story "' + storyName + '" was not found. Did you export it?');
+        return;
+    } else {
+        var element = typeof Component === 'function'
+            ? React.createElement(Component)
+            : Component;
+
+        ReactDOM.render(element, rootEl);
+    }
+}
+
+window.addEventListener('hashchange', function() {
+    ReactDOM.unmountComponentAtNode(rootEl);
+    renderStory();
+});
+
+renderStory();
--- rainbow/src/ui/nav/story.js
+++ rainbow/src/ui/nav/story.js
@@ -2,3 +2,5 @@ import React from 'react';
 import Nav from './';
 
 export default <Nav />;
+
+export const withHeading = <Nav>Rainbow</Nav>;
