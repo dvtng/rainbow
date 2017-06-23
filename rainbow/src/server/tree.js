47a0f375e6a95e44ee877e2629397b99d65b0034
Joshua Tjhin <xuheng.tjhin@gmail.com>
2017-06-23 18:04:18 +1000

    Add tree to turn paths into tree structure

---
+++ rainbow/src/server/tree.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+
+const tree = files => {
+    const createTree = (node, path) => {
+        if (!path.length) {
+            return;
+        }
+
+        node.children = node.children || [];
+        const fragment = path.shift();
+        let child = _.find(node.children, child => child.name === fragment);
+
+        if (!child) {
+            child = {
+                name: fragment
+            };
+            node.children.push(child);
+        }
+
+        createTree(child, path);
+    };
+
+    const root = {
+        name: 'root'
+    };
+
+    const paths = _.map(files, file =>
+        file.split('/').filter(name => name.length)
+    );
+
+    _.each(paths, path => createTree(root, path));
+
+    return root;
+};
+
+export default tree;
--- rainbow/src/server/tree.test.js
+++ rainbow/src/server/tree.test.js
@@ -1,8 +1,92 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import tree from './tree.js';
 
-describe('tree', () => {
-    it('should work', () => {
-        expect(1).to.equal(1);
+describe('tree test', () => {
+    it('should turn single path into tree', () => {
+        const paths = ['/dir1/dir2/foo.js'];
+
+        const expectedTree = {
+            name: 'root',
+            children: [
+                {
+                    name: 'dir1',
+                    children: [
+                        {
+                            name: 'dir2',
+                            children: [
+                                {
+                                    name: 'foo.js'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+        expect(tree(paths)).to.deep.equal(expectedTree);
+    });
+
+    it('should turn multiple paths into tree', () => {
+        const paths = ['/dir1/dir2/foo.js', '/dir1/bar.js', 'cat.js'];
+
+        const expectedTree = {
+            name: 'root',
+            children: [
+                {
+                    name: 'dir1',
+                    children: [
+                        {
+                            name: 'dir2',
+                            children: [
+                                {
+                                    name: 'foo.js'
+                                }
+                            ]
+                        },
+                        {
+                            name: 'bar.js'
+                        }
+                    ]
+                },
+                {
+                    name: 'cat.js'
+                }
+            ]
+        };
+        expect(tree(paths)).to.deep.equal(expectedTree);
+    });
+
+    it('should turn paths with depth of 3 into tree', () => {
+        const paths = ['/dir1/dir2/dir3/foo.js', '/dir1/dir2/bar.js'];
+
+        const expectedTree = {
+            name: 'root',
+            children: [
+                {
+                    name: 'dir1',
+                    children: [
+                        {
+                            name: 'dir2',
+                            children: [
+                                {
+                                    name: 'dir3',
+                                    children: [
+                                        {
+                                            name: 'foo.js'
+                                        }
+                                    ]
+                                },
+                                {
+                                    name: 'bar.js'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+
+        expect(tree(paths)).to.deep.equal(expectedTree);
     });
 });
