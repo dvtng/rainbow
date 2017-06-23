e861eebed627ef894d608837e1dcac6edadcfc9d
David Tang <tang.ddt@gmail.com>
2017-06-23 23:52:35 +1000

    Add logo

--- rainbow/src/ui/nav/nav.js
+++ rainbow/src/ui/nav/nav.js
@@ -5,10 +5,24 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     background-color: #202020;
+    box-sizing: border-box;
     min-height: 400px;
+    padding: 32px;
     width: 338px;
 `;
 
-const Nav = ({ root }) => <Container><Explorer root={root} /></Container>;
+const H1 = styled.h1`
+    color: #fff;
+    font-size: 32px;
+    font-family: 'Lily Script One', cursive;
+    font-weight: normal;
+    margin: 0;
+`;
+
+const Nav = ({ root }) =>
+    <Container>
+        <H1>Rainbow</H1>
+        <Explorer root={root} />
+    </Container>;
 
 export default Nav;
--- rainbow/src/ui/typography.js
+++ rainbow/src/ui/typography.js
@@ -1,4 +1,16 @@
-import styled from 'styled-components';
+import styled, { injectGlobal } from 'styled-components';
+
+injectGlobal`
+    @font-face {
+        font-family: 'Lily Script One';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Lily Script One'),
+             local('LilyScriptOne-Regular'),
+             url('/lily-script-one/LilyScriptOne-Regular.ttf') format('truetype');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
+    }
+`;
 
 export default styled.div`
     font-family: sans-serif;
