8b6af9440048fa6687cdf331ca93d961598dbf82
David Tang <tang.ddt@gmail.com>
2017-06-25 19:13:46 +1000

    Split file info into 2 lines

--- rainbow/src/ui/nav/story-nav/file-info.js
+++ rainbow/src/ui/nav/story-nav/file-info.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import { inject, observer } from 'mobx-react';
 
-export const FileInfo = ({ file }) =>
-    <div>
-        <span>{file}</span>
-    </div>;
+const pattern = /(.*)\/(.*)/;
+const split = file => pattern.exec(file);
+
+const Path = styled.div`
+    color: #808080;
+    margin-bottom: 8px;
+`;
+
+const File = styled.div`
+    margin-bottom: 8px;
+`;
+
+export const FileInfo = ({ file }) => {
+    if (!file) return null;
+
+    const parts = split(file);
+    return (
+        <div>
+            <Path>{parts[1]}</Path>
+            <File>{parts[2]}</File>
+        </div>
+    );
+};
 
 export default inject(stores => ({
     file: stores.nav.selectedFile
