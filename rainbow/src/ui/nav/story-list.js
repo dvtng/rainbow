bca5aee9878db68d5d411245cbf9a96c8c28cd7d
David Tang <tang.ddt@gmail.com>
2017-06-25 12:25:40 +1000

    Give background color to selected story

--- rainbow/src/ui/nav/story-list.js
+++ rainbow/src/ui/nav/story-list.js
@@ -3,6 +3,8 @@ import { inject, observer } from 'mobx-react';
 import styled from 'styled-components';
 
 const StoryItem = styled.a`
+    background-color: ${props =>
+        props.isSelected ? '#606060' : 'transparent'};
     border-left: 4px solid;
     border-color: ${props => (props.isSelected ? '#C4C4C4' : 'transparent')};
     cursor: pointer;
@@ -10,7 +12,7 @@ const StoryItem = styled.a`
     padding: 12px 16px;
 
     &:hover {
-        background: #505050;
+        background-color: #505050;
     }
 `;
 
