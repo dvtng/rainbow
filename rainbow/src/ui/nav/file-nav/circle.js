008ca8b6518e04aa4061fc605a2a8c353ac18e02
David Tang <tang.ddt@gmail.com>
2017-06-26 23:08:52 +1000

    Half-done file tree branches

---
+++ rainbow/src/ui/nav/file-nav/circle.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { circleSize } from './sizes';
+
+const Circle = styled.div`
+    border: 1px solid;
+    border-color: ${props => (props.isSelected ? '#fff' : '#999')};
+    border-radius: 50%;
+    box-sizing: border-box;
+    display: inline-block;
+    height: ${circleSize}px;
+    margin-right: 8px;
+    width: ${circleSize}px;
+    vertical-align: bottom;
+`;
+
+export default Circle;
