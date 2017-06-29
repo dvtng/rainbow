9ebd731d988546febd8d1a6af61b964319f0f246
David Tang <tang.ddt@gmail.com>
2017-06-29 17:13:09 +1000

    Add import rules to eslint

--- rainbow/src/compiler/index.js
+++ rainbow/src/compiler/index.js
@@ -1,4 +1,3 @@
 exports.compileLibs = require('./compile-libs');
-exports.compileStory = require('./compile-story');
 exports.getStoryFiles = require('./get-story-files');
 exports.storyDevServer = require('./story-dev-server');
